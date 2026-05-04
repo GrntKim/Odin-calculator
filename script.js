e9104472a720ef8ef0cf4db5b4019b348586e7e3
GrntKim <grantjun0224@gmail.com>
2026-05-04 14:08:16 +0900

    Add event listeners to each button

---
+++ script.js
@@ -0,0 +1,33 @@
+let num1 = null;
+let num2 = null;
+let result = null;
+
+const operationButtons = document.querySelectorAll('.calc-btn--operator');
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const op = button.dataset.action;
+        console.log(op);
+    });
+});
+
+const controlButtons = document.querySelectorAll('.calc-btn--control');
+controlButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const op = button.dataset.action;
+        console.log(op);
+    });
+});
+
+const numberButtons = document.querySelectorAll('.calc-btn--number');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const num = button.dataset.value;
+        console.log(num);
+    });
+});
+
+const resultButton = document.querySelector('.calc-btn--result');
+resultButton.addEventListener('click', () => {
+    const op = resultButton.dataset.action;
+    console.log(op);
+});
